0c6eebf1b68fc2fe8599cc624cdb3d15a318f539
ahmedhoussem <ahmedhoussem.assal@esprit.tn>
2019-04-27 18:23:06 +0100

    Add files via upload

---
+++ RandomFloat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+[Serializable]
+public class RandomFloat
+{
+    static readonly Random _seed = new Random();
+
+    public float Max;
+    public float Min;
+
+    [UnityEngine.Tooltip("Set this to true if you want a constant value instead of a new value each time")]
+    public bool OneShotMode;
+
+    private bool isSet;
+    private float OneShotValue;
+
+    public float Value
+    {
+        get
+        {
+            if (!OneShotMode)
+                return (float)(_seed.NextDouble() * (Max - Min) + Min);
+            if (!isSet)
+            {
+                OneShotValue = (float)(_seed.NextDouble() * (Max - Min) + Min);
+                isSet = true;
+            }
+            return OneShotValue;
+
+        }
+    }
+
+
+    public RandomFloat(float MinValue, float MaxValue, bool isOneShotMode)
+    {
+        Max = MaxValue;
+        Min = MinValue;
+        OneShotMode = isOneShotMode;
+    }
+
+    public static implicit operator float(RandomFloat d)
+    {
+        return d.Value;
+    }
+}
---
+++ RandomInt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+[Serializable]
+public class RandomInt
+{
+    static readonly Random _seed = new Random();
+
+    public int Max;
+    public int Min;
+
+    [UnityEngine.Tooltip("Set this to true if you want a constant value instead of a new value each time")]
+    public bool OneShotMode;
+
+    private bool isSet;
+    private int OneShotValue;
+
+    public int Value
+    {
+        get
+        {
+            if (!OneShotMode)
+                return _seed.Next(Min, Max);
+            if (!isSet)
+            { 
+                OneShotValue = _seed.Next(Min, Max);
+                isSet = true;
+            }
+            return OneShotValue;
+
+        }
+    }
+
+
+    public RandomInt(int MinValue, int MaxValue, bool isOneShotMode)
+    {
+        Max = MaxValue;
+        Min = MinValue;
+        OneShotMode = isOneShotMode;
+    }
+
+    public static implicit operator int(RandomInt d)
+    {
+        return d.Value;
+    }
+}
